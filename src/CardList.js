3299f468fc8d295b6b8fbb74a059720e558c9647
Boggy92 <zhilebre@gmail.com>
2020-07-18 22:24:54 +0200

    fixed some issue

--- src/CardList.js
+++ src/CardList.js
@@ -1,27 +1,23 @@
 import React from 'react'
-import Card from './Card.js'
+import Card from './Card'
 
 const CardList = ({ robots }) => {
-//    nema potrebe za ovim if
-//    if(true) {
-//        throw new Error('Noooooooo')
-//    }
-    const cardsArray = robots.map((user,i) =>{
-        return (
-            <Card 
-                key={user.id} 
-                id={robots[i].id} 
-                name={robots[i].name} 
-                email={robots[i].email}
-            />
-        )
-    })
-    
-    return (
-        <div>
-            {cardsArray}    
-        </div>
-    )
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
 }
 
 export default CardList 
